567046a90c11ac9dc43adfa115c5ef724cc27669
coder71 <kamrunnaher877@gmail.com>
2022-01-04 19:59:02 +0600

    order crud operation complete

--- index.js
+++ index.js
@@ -64,6 +64,24 @@ async function run() {
       }
     });
 
+    // (READ) --> GET ALL THE SPECIFIQ ORDER INFO OR ALL ORDERS OF A USER VIA QUERYING
+    app.get('/orders', async (req, res, next) => {
+      try {
+        let orders; // find all orders
+
+        if (req.query?.email) {
+          const email = req.query.email;
+          orders = await Order.find({ email }); // find the specific user order
+        } else {
+          orders = await Order.find(); // find all the orders
+        }
+
+        res.json(orders); // send the orders to client side.
+      } catch (error) {
+        next(error);
+      }
+    });
+
     // (READ) --> FIND A USER IS ADMIN OR NOT
     app.get('/user', async (req, res, next) => {
       try {
@@ -116,49 +134,81 @@ async function run() {
       }
     });
 
+    //(UPDATE) --> UPDATE THE ORDER STATUS
+    app.put('/orders/:id', async (req, res, next) => {
+      try {
+        const id = req.params.id;
+
+        const filter = { _id: ObjectId(id) };
+
+        const options = { upsert: true };
+
+        // update the order status
+        const updateOrder = {
+          $set: {
+            status: 'shipped',
+          },
+        };
+
+        const result = await Order.updateOne(filter, updateOrder, options);
+
+        res.json(result); // send the response to client
+      } catch (error) {
+        next(error);
+      }
+    });
+
     //(UPDATE) --> UPDATE AN USER
-    app.put('/user', async (req, res) => {
-      const user = req.body;
+    app.put('/user', async (req, res, next) => {
+      try {
+        const user = req.body;
 
-      const filter = { email: user.email };
+        const filter = { email: user.email };
 
-      const options = { upsert: true };
+        const options = { upsert: true };
 
-      const updateDoc = { $set: user };
+        const updateDoc = { $set: user };
 
-      const result = await User.updateOne(filter, updateDoc, options);
+        const result = await User.updateOne(filter, updateDoc, options);
 
-      res.json(result); // send the respone to client side
+        res.json(result); // send the respone to client side
+      } catch (error) {
+        next(error);
+      }
     });
 
     //(UPDATE) --> UPDATE THE USER ROLE
-    app.put('/user/admin', async (req, res) => {
-      const user = req.body; // will come from frontend ({requester: email, newAdminEmail: email})
+    app.put('/user/admin', async (req, res, next) => {
+      try {
+        const user = req.body; // will come from frontend ({requester: email, newAdminEmail: email})
 
-      if (user?.requester) {
-        const requesterAccount = await User.findOne({
-          email: user.requester,
-        }); // find the requester info in database
+        if (user?.requester) {
+          const requesterAccount = await User.findOne({
+            email: user.requester,
+          }); // find the requester info in database
 
-        // check if the requester is admin or not
-        if (requesterAccount?.role === 'admin') {
-          const filter = { email: user.newAdminEmail };
+          // check if the requester is admin or not
+          if (requesterAccount?.role === 'admin') {
+            const filter = { email: user.newAdminEmail };
 
-          const updateDoc = { $set: { role: 'admin' } };
+            const updateDoc = { $set: { role: 'admin' } };
 
-          const result = await User.updateOne(filter, updateDoc);
+            const result = await User.updateOne(filter, updateDoc);
 
-          res.json(result); // send the result after updating an user role
+            res.json(result); // send the result after updating an user role
+          } else {
+            res
+              .status(403)
+              .json({ message: 'you do not have access to make admin' });
+          }
         } else {
-          res
-            .status(403)
-            .json({ message: 'you do not have access to make admin' });
+          res.status(404).json({
+            message:
+              'Please make sure the user that you want to make admin is available in database.',
+          });
         }
-      } else {
-        res.status(404).json({
-          message:
-            'Please make sure the user that you want to make admin is available in database.',
-        });
+      } catch (error) {
+        next(error);
       }
     });
 
@@ -172,6 +222,36 @@ async function run() {
         const result = await Product.deleteOne(query); // delete the matched product from database
 
         res.json(result); // send the response to client side
+      } catch (error) {
+        next(error);
+      }
+    });
+
+    // (DELETE) --> DELETE AN ORDER FROM THE DATABASE
+    app.delete('/orders/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const query = { _id: ObjectId(id) };
+
+        const result = await Order.deleteOne(query); // delete the matched order from database
+
+        res.json(result); // send the response to user
+      } catch (error) {
+        next(error);
+      }
+    });
+
+    // (DELETE) --> DELETE ALL ORDERS WITH SPECIFIC ID
+    app.delete('/orders/deleteall/:id', async (req, res) => {
+      try {
+        const id = req.params.id;
+
+        const query = { product_id: id };
+
+        const result = await Order.deleteMany(query); // delete all the matched order from database
+
+        res.json(result); // send the response to user
       } catch (error) {
         next(error);
       }
